0a1c5d23acbf849ed83da5d6e8e83555e358e688
Fateme Lashkari <fatemelashkariii84@gamil.com>
2024-03-12 20:16:05 +0330

    something has changed

--- src/main/java/Exercises1.java
+++ src/main/java/Exercises1.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 public class Exercises1 {
     /*
     implement a function that returns factorial of given n
      */
     public long factorial(int n) {
-        // TODO
-        return 0;
+        if(n == 1 || n == 0){
+            return 1;
+        }
+        else{
+            return n * factorial(n-1);
+        }
     }
 
 
@@ -13,8 +18,15 @@ public class Exercises1 {
     the series -> 1, 1, 2, 3, 5, 8, ...
     */
     public long fibonacci(int n) {
-        // TODO
-        return 0;
+        if(n==1){
+            return 1;
+        }
+        else if(n==2){
+            return 1;
+        }
+        else{
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
     }
 
 
@@ -30,13 +42,38 @@ public class Exercises1 {
 
      */
     public char[][] generateTriangle(int rows) {
-        // TODO
-        return null;
+        char[][] triangle = new char[rows][];
+        for (int i = 0; i < rows; i++) {
+            triangle[i] = new char[i + 1]; // Each row has i + 1 columns
+            for (int j = 0; j <= i; j++) {
+                triangle[i][j] = '*'; // Fill each position with '*'
+            }
+        }
+        return triangle;
     }
 
 
     public static void main(String[] args) {
-        // test your code here!
+        Scanner in = new Scanner(System.in);
+        while(true){
+            int n;
+            n = in.nextInt();
+            Exercises1 obj = new Exercises1();
+            System.out.println(obj.factorial(n));
+            n = in.nextInt();
+            System.out.println(obj.fibonacci(n));
+            //for triangle
+            n = in.nextInt();
+            char[][] triangle = obj.generateTriangle(n);
+            for (int i = 0 ; i < triangle.length ; i++) {
+                for (int j = 0 ; j < triangle[i].length ; j++) {
+                    System.out.print(triangle[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+        }
+
     }
 
 }
